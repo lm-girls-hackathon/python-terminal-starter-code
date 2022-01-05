6c3505934b711acf94a7988f014dc6edca38cc1a
Sasha <74149971+EwwPhysics@users.noreply.github.com>
2022-01-05 12:04:40 -0500

    Update with added comments and less given code

--- hangman.py
+++ hangman.py
@@ -1,22 +1,30 @@
-WORD = input("enter the word")
+def play_game():
+    word = input("enter the word")
+    guess = ["_" for _ in range(len(word))]  # create list of characters which represents the word
+    # TODO: create a strike counter which counts how many times they guess incorrectly
 
-guess = ["_" for _ in range(len(WORD))]
+    while "".join(guess) != word:
+        letter = input("guess a letter")  # the user will enter a letter to guess
+        # TODO: verify that the user entered a valid input (a single letter)
 
-# TODO: create a strike counter which counts how many times they guess incorrectly
+        if letter in word:
+            update_guess(guess, word, letter)
+        else:
+            # TODO: update the strike counter if an incorrect letter was guessed
+            ...
 
-while "".join(guess) != WORD:
-    letter = input("guess a letter")
-    if letter in WORD:
-        for i in range(len(WORD)):
-            if WORD[i] == letter:
-                guess[i] = letter
+        # TODO: exit the program if there are a certain number of strikes
+        print(" ".join(guess))  # prints the guess as a space-separated string
+        # TODO: print a list of letters which are not in the word
 
-    # TODO: update the strike counter if an incorrect letter was guessed
-    # TODO: exit the program if there are a certain number of strikes
+    # TODO: make a message congratulating the player if they win
+    # TODO: make a loop that lets users play another game
+    # TODO: draw a hangman figure using keyboard characters
 
-    print(" ".join(guess))
+    
+def update_guess(guess, word, letter):
+    # TODO: update guess based on where the letter is in the word
+    ...
 
 
-# TODO: make a message congratulating the player if they win
-# TODO: make a loop that lets users play another game
-# TODO: draw a hangman figure using keyboard characters
+play_game()
